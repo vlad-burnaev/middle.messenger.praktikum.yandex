247445d40b5f9dc3368da2659b3f2e9a7f07c110
Vladislav Burnaev <vlad-burn@yandex-team.ru>
2022-02-18 02:39:18 +0300

    sprint_1 review: new lines

--- src/pages/Main/Main.data.js
+++ src/pages/Main/Main.data.js
@@ -3,63 +3,79 @@ export const MainPageData = {
         {
             name: 'Андрей',
             lastMessage: { text: 'Изображение', prefix: '' },
-            meta: { time: '10:49', newMessagesCount: 2 }
+            meta: { time: '10:49', newMessagesCount: 2 },
         },
         {
             name: 'Киноклуб',
             lastMessage: { text: 'Стикер', prefix: 'Вы:' },
-            meta: { time: '12:00', newMessagesCount: 0 }
+            meta: { time: '12:00', newMessagesCount: 0 },
         },
         {   name: 'Илья',
-            lastMessage: { text: 'Друзья, у меня для вас особенный выпуск новостей! Еще какой-то текст', prefix: '' },
-            meta: { time: '15:12', newMessagesCount: 4 }
+            lastMessage: {
+            text: 'Друзья, у меня для вас особенный выпуск новостей! Еще какой-то текст',
+                prefix: ''
+            },
+            meta: { time: '15:12', newMessagesCount: 4 },
         },
         {   name: 'Вадим',
             lastMessage: { text: 'Круто', prefix: '\'Вы:' },
-            meta: { time: 'Пт', newMessagesCount: 0 }
+            meta: { time: 'Пт', newMessagesCount: 0 },
         },
         {   name: 'тет-а-теты',
-            lastMessage: { text: 'И Human Interface Guidelines и Material Design рекомендуют еще какой-то текст', prefix: '' },
-            meta: { time: 'Ср', newMessagesCount: 0 }
+            lastMessage: {
+                text: 'И Human Interface Guidelines и Material Design рекомендуют еще какой-то текст',
+                prefix: ''
+            },
+            meta: { time: 'Ср', newMessagesCount: 0 },
         },
         {   name: '1, 2, 3',
-            lastMessage: { text: 'Миллионы россиян ежедневно проводят десятки часов своего времени в интырнетах', prefix: '' },
-            meta: { time: 'Пн', newMessagesCount: 0 }
+            lastMessage: {
+                text: 'Миллионы россиян ежедневно проводят десятки часов своего времени в интырнетах',
+                prefix: ''
+            },
+            meta: { time: 'Пн', newMessagesCount: 0 },
         },
         {   name: 'Design Destroyer',
-            lastMessage: { text: 'В 2008 году художник Jon Rafman  начал собирать бутылки', prefix: '' },
-            meta: { time: 'Пн', newMessagesCount: 0 }
+            lastMessage: {
+                text: 'В 2008 году художник Jon Rafman  начал собирать бутылки',
+                prefix: ''
+            },
+            meta: { time: 'Пн', newMessagesCount: 0 },
         },
         {   name: 'Day.',
-            lastMessage: { text: 'Так увлёкся работой по курсу, что совсем забыл его анонсировал что-то', prefix: '' },
-            meta: { time: '1 Мая 2020', newMessagesCount: 0 }
+            lastMessage: {
+                text: 'Так увлёкся работой по курсу, что совсем забыл его анонсировал что-то',
+                prefix: ''
+            },
+            meta: { time: '1 Мая 2020', newMessagesCount: 0 },
         },
         {   name: 'Стас Рогозин',
             lastMessage: { text: 'Можно или сегодня или завтра вечером.', prefix: '' },
-            meta: { time: '12 Апр 2020', newMessagesCount: 0 }
+            meta: { time: '12 Апр 2020', newMessagesCount: 0 },
         },
         {   name: 'Стас Рогозин',
             lastMessage: { text: 'Можно или сегодня или завтра вечером.', prefix: '' },
-            meta: { time: '12 Апр 2020', newMessagesCount: 0 }
+            meta: { time: '12 Апр 2020', newMessagesCount: 0 },
         },
         {   name: 'Стас Рогозин',
             lastMessage: { text: 'Можно или сегодня или завтра вечером.', prefix: '' },
-            meta: { time: '12 Апр 2020', newMessagesCount: 0 }
+            meta: { time: '12 Апр 2020', newMessagesCount: 0 },
         },
         {   name: 'Стас Рогозин',
             lastMessage: { text: 'Можно или сегодня или завтра вечером.', prefix: '' },
-            meta: { time: '12 Апр 2020', newMessagesCount: 0 }
+            meta: { time: '12 Апр 2020', newMessagesCount: 0 },
         },
         {   name: 'Стас Рогозин',
             lastMessage: { text: 'Можно или сегодня или завтра вечером.', prefix: '' },
-            meta: { time: '12 Апр 2020', newMessagesCount: 0 }
+            meta: { time: '12 Апр 2020', newMessagesCount: 0 },
         },
         {   name: 'Стас Рогозин',
             lastMessage: { text: 'Можно или сегодня или завтра вечером.', prefix: '' },
-            meta: { time: '12 Апр 2020', newMessagesCount: 0 }
-        },{   name: 'Стас Рогозин',
+            meta: { time: '12 Апр 2020', newMessagesCount: 0 },
+        },
+        {   name: 'Стас Рогозин',
             lastMessage: { text: 'Можно или сегодня или завтра вечером.', prefix: '' },
-            meta: { time: '12 Апр 2020', newMessagesCount: 0 }
+            meta: { time: '12 Апр 2020', newMessagesCount: 0 },
         },
     ],
     dialog: {
@@ -156,15 +172,15 @@ export const MainPageData = {
                             meta: {
                                 status: 'sent',
                                 time: '15:04'
-                            }
+                            },
                         },
                         {
                             text: 'Как дела?',
                             meta: {
                                 status: 'sent',
                                 time: '15:04'
-                            }
-                        }
+                            },
+                        },
                     ]
                 },
                 {
@@ -176,15 +192,15 @@ export const MainPageData = {
                             meta: {
                                 status: 'sent',
                                 time: '16:05'
-                            }
+                            },
                         },
                         {
                             text: 'Как в школе?',
                             meta: {
                                 status: 'sent',
                                 time: '16:06'
-                            }
-                        }
+                            },
+                        },
                     ]
                 },
                 {
@@ -204,7 +220,7 @@ export const MainPageData = {
                                 status: 'sent',
                                 time: '15:04'
                             }
-                        }
+                        },
                     ]
                 },
                 {
@@ -224,7 +240,7 @@ export const MainPageData = {
                                 status: 'sent',
                                 time: '15:06'
                             }
-                        }
+                        },
                     ]
                 },
                 {
@@ -244,7 +260,7 @@ export const MainPageData = {
                                 status: 'sent',
                                 time: '13:04'
                             }
-                        }
+                        },
                     ]
                 },
             ]
