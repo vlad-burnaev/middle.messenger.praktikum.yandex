32f7d3e0dff08c110f0399e966c383323a19ad39
Vladislav Burnaev <vlad-burn@yandex-team.ru>
2022-02-18 02:34:13 +0300

    sprint_1 review: clear server

--- server.js
+++ server.js
@@ -5,26 +5,6 @@ const PORT = 3000;
 
 app.use(express.static('./dist'));
 
-const API_PREFIX = '/api/v1';
-
-app.post(`${API_PREFIX}/test`, (req, res) => {
-    res.status(201).send('my text');
-});
-
-app.delete(`${API_PREFIX}/delete`, (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).send({deleted: true});
-});
-
-app.get(`${API_PREFIX}/text`, (req, res) => {
-    res.status(200).send('Hello, World!');
-});
-
-app.put(`${API_PREFIX}/json`, (req, res) => {
-    res.setHeader('content-type', 'application/json');
-    res.status(201).send({data: {items: [1, 2, 3]}});
-});
-
 app.listen(PORT, () => {
     console.log(`Проект запущен! Чекай http://localhost:${PORT}`);
 });
