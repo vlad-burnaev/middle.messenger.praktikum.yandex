a35b536ce1651ae6c77e9a219e2c27a8659316c8
Vladislav Burnaev <vlad-burn@yandex-team.ru>
2022-02-20 06:19:48 +0300

    Handlebbars refactoring: RegisterPartial

--- src/pages/ProfilePages/ProfileChangePassword/ProfileChangePassword.ts
+++ src/pages/ProfilePages/ProfileChangePassword/ProfileChangePassword.ts
@@ -1,15 +1,9 @@
-import {ProfileAvatar} from "../components/ProfileAvatar/ProfileAvatarTemplate";
-import {ProfileGoBack} from "../components/ProfileGoBack/ProfileGoBackTemplate";
-import {ProfileDataFieldEditable} from "../components/ProfileDataFieldEditable/ProfileDataFieldEditableTemplate";
 import {profileChangePasswordPageTemplate} from "./ProfileChangePassword.template";
-import {Button} from "../../../components/Button/ButtonTemplate";
 import {profileChangePasswordPageData} from "./ProfileChangePassword.data";
 import Handlebars from "handlebars";
 
 const container = document.querySelector('.ProfileChangePasswordPage');
 
-Handlebars.registerPartial({ profileAvatar: ProfileAvatar, profileDataFieldEditable: ProfileDataFieldEditable, button: Button, profileGoBack: ProfileGoBack });
+const ProfileChangePasswordPage = Handlebars.compile(profileChangePasswordPageTemplate);
 
-const profileChangePasswordPage = Handlebars.compile(profileChangePasswordPageTemplate);
-
-container.innerHTML = profileChangePasswordPage(profileChangePasswordPageData);
+container.innerHTML = ProfileChangePasswordPage(profileChangePasswordPageData);
--- src/pages/ProfilePages/ProfileEditData/ProfileEditData.ts
+++ src/pages/ProfilePages/ProfileEditData/ProfileEditData.ts
@@ -1,15 +1,9 @@
-import {ProfileAvatar} from "../components/ProfileAvatar/ProfileAvatarTemplate";
-import {ProfileGoBack} from "../components/ProfileGoBack/ProfileGoBackTemplate";
-import {ProfileDataFieldEditable} from "../components/ProfileDataFieldEditable/ProfileDataFieldEditableTemplate";
 import {profileEditDataPageTemplate} from "./ProfileEditData.template";
 import {profileEditDataPageData} from "./ProfileEditData.data";
-import {Button} from "../../../components/Button/ButtonTemplate";
 import Handlebars from "handlebars";
 
 const container = document.querySelector('.ProfileEditData');
 
-Handlebars.registerPartial({ profileAvatar: ProfileAvatar, profileDataFieldEditable: ProfileDataFieldEditable, button: Button, profileGoBack: ProfileGoBack });
+const ProfileEditDataPage = Handlebars.compile(profileEditDataPageTemplate);
 
-const profileEditDataPage = Handlebars.compile(profileEditDataPageTemplate);
-
-container.innerHTML = profileEditDataPage(profileEditDataPageData);
+container.innerHTML = ProfileEditDataPage(profileEditDataPageData);
