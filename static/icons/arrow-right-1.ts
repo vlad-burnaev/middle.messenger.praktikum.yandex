ee0d9b794a8d8f499e2a1c09ec905a1bd9589dd6
Vladislav Burnaev <vlad-burn@yandex-team.ru>
2022-03-24 00:37:52 +0300

    icons refactoring

--- static/icons/arrow-right-1.ts
+++ static/icons/arrow-right-1.ts
@@ -1,4 +1,4 @@
-export const ArrowRight1Icon = `
+export const ArrowRight1 = `
     <svg width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M1 9L5 5L1 1" stroke="#999"/>
     </svg>
