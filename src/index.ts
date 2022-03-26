fe01342588400ba79c19005825ee4a320e915c26
Vladislav Burnaev <vlad-burn@yandex-team.ru>
2022-03-26 16:08:08 +0300

    navigation page

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,9 @@
 import { renderDOM } from './utils/renderDOM';
 import './styles/index.pcss';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Error404 from './pages/404';
-import Error500 from './pages/500';
-import ProfileMain from './pages/Profile/Main';
-import ProfileEditData from './pages/Profile/EditData';
-import ProfileChangePassword from './pages/Profile/ChangePassword';
-import Main from './pages/Main';
+import Navigation from './pages/navigation';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const signIn = new SignIn();
-  const signUp = new SignUp();
-  const error404 = new Error404();
-  const error500 = new Error500();
-  const profileMain = new ProfileMain();
-  const profileEditData = new ProfileEditData();
-  const profileChangePassword = new ProfileChangePassword();
-  const main = new Main();
+  const navigation = new Navigation();
 
-  renderDOM('#app', main);
+  renderDOM('#app', navigation);
 });
