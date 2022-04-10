4889868a16bf0d182acba88e2b5b202575fe0b8a
Vladislav Burnaev <vlad-burn@yandex-team.ru>
2022-04-11 01:24:04 +0300

    HTTPRequest

---
+++ src/utils/HTTPRequest.ts
@@ -0,0 +1,81 @@
+const Methods = {
+  GET: 'GET',
+  POST: 'POST',
+  PUT: 'PUT',
+  DELETE: 'DELETE',
+};
+
+function queryStringify(data: any) {
+  if (typeof data !== 'object') {
+    throw new Error('Data must be object');
+  }
+
+  const keys = Object.keys(data);
+  return keys.reduce((result, key, index) => `${result}${key}=${data[key]}${index < keys.length - 1 ? '&' : ''}`, '?');
+}
+
+interface Options {
+  method: string,
+  timeout?: number,
+  data?: object,
+  headers?: object
+}
+export class HTTPTransport {
+  get = (url: string, options: Options) => {
+    this.request(url, { ...options, method: Methods.GET }, options.timeout);
+  };
+
+  post = (url: string, options: Options) => {
+    this.request(url, { ...options, method: Methods.POST }, options.timeout);
+  };
+
+  put = (url: string, options: Options) => {
+    this.request(url, { ...options, method: Methods.PUT }, options.timeout);
+  };
+
+  delete = (url: string, options: Options) => {
+    this.request(url, { ...options, method: Methods.DELETE }, options.timeout);
+  };
+
+  request = (url: string, options: Options, timeout = 5000) => {
+    const { headers = {}, method, data } = options;
+
+    return new Promise(((resolve, reject) => {
+      if (!method) {
+        reject('No method');
+        return;
+      }
+
+      const xhr = new XMLHttpRequest();
+      const isGet = method === Methods.GET;
+
+      xhr.open(
+        method,
+        isGet && !!data
+          ? `${url}${queryStringify(data)}`
+          : url,
+      );
+
+      Object.keys(headers).forEach((key) => {
+        xhr.setRequestHeader(key, headers[key]);
+      });
+
+      xhr.onload = function () {
+        resolve(xhr);
+      };
+
+      xhr.onabort = reject;
+      xhr.onerror = reject;
+
+      xhr.timeout = timeout;
+      xhr.ontimeout = reject;
+
+      if (isGet || !data) {
+        xhr.send();
+      } else {
+        // @ts-ignore
+        xhr.send(data);
+      }
+    }));
+  };
+}
